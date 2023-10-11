bc6483fae9298c5b3e78df223325fb05e4b25d3a
lucy.li <lucy.li@quectel.com>
2023-10-11 21:26:48 +0800

    feat: 配置 swagger & validationPipe

---
+++ src/helper/doc.ts
@@ -0,0 +1,18 @@
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import * as packageConfig from '../../package.json';
+export const generateDocument = (app) => {
+  console.log(app);
+  const options = new DocumentBuilder()
+    .setTitle(packageConfig.name)
+    .setDescription(packageConfig.description)
+    .setVersion(packageConfig.version)
+    .addBearerAuth({
+      type: 'http',
+      name: 'jwt',
+      description: 'bearer',
+    })
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/doc', app, document);
+};
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,19 @@ import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptor/transform.interceptor';
 import { BaseExceptionsFilter } from './common/exceptions/base.exceptions.filter';
 import { HttpExceptionsFilter } from './common/exceptions/http.exceptions.filter';
+import { generateDocument } from './helper';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // 全局数据转换
   app.useGlobalInterceptors(new TransformInterceptor());
+  // 全局错误拦截
   app.useGlobalFilters(new BaseExceptionsFilter(), new HttpExceptionsFilter());
+  // 启动全局 DTO 参数检查
+  app.useGlobalPipes(new ValidationPipe());
+  // swagger 文档
+  generateDocument(app);
   await app.listen(3000);
 }
 bootstrap();
